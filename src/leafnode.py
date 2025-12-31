5d37d70f56ea77312ce3b82b4ad79c5c457ddb9b
blacksagres <geraldo.nogueirabs@gmail.com>
2025-12-31 13:25:33 +0100

    chore: add to_html function to leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -9,3 +9,15 @@ class LeafNode(HTMLNode):
         props: dict[str, str] | None = None,
     ) -> None:
         super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        # If the leaf node has no value, it should raise a ValueError. All leaf nodes must have a value.
+        # If there is no tag (e.g. it's None), the value should be returned as raw text.
+
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}>{self.value}</{self.tag}>"
