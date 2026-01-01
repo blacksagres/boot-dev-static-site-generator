7daca0caf14af42724a910e1a715f4a8f2856fdc
blacksagres <geraldo.nogueirabs@gmail.com>
2026-01-01 12:16:07 +0100

    chore: add new utils function to split text nodes

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,39 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode("img", "", {"src": normalized_src, "alt": text_node.text})
 
     raise ValueError(f"{text_node.text_type} this text type is not supported.")
+
+
+def split_nodes_delimiter(text: list[TextNode], delimiter: str, text_type: TextType):
+    """
+    Creates {TextNode} from string.
+
+    ---
+
+    Example:
+
+    Input: `"This is text with a **bolded phrase** in the middle"`
+
+    Output:
+
+    ```
+    [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("bolded phrase", TextType.BOLD),
+        TextNode(" in the middle", TextType.TEXT),
+    ]
+
+    ```
+
+    :param text: A list of text nodes which will be checked
+    for their delimiter.
+    :type text: str
+
+    :param delimiter: The delimiter to split the text up,
+    for example code: '`', bold: `**`, etc.
+    :type delimiter: str
+
+    :param text_type: The text node type which will be searched in the
+    text nodes to be split.
+    """
+
+    raise NotImplementedError()
