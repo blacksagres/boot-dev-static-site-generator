9c0c677f8a28b2f43016ccb30e5e2752075530a0
blacksagres <geraldo.nogueirabs@gmail.com>
2026-01-01 12:44:48 +0100

    chore: add test for invalid delimiter

--- src/test_utils.py
+++ src/test_utils.py
@@ -5,6 +5,9 @@ from utils import text_node_to_html_node
 
 
 class TestUtils(unittest.TestCase):
+
+    # text_node_to_html_node
+
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -21,3 +24,11 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(
             html_node.props, {"src": "https://scryfall.com", "alt": node.text}
         )
+
+    # split_nodes_delimiter
+
+    def test_split_nodes_delimiter_raises_if_delimiter_not_found(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "impossible_delimiter", TextType.CODE)
+
